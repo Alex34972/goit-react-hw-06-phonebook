ac79343e5b2d91dff0b3f6d2005471443af046c8
Alex34972 <alex_hifi@ukr.net>
2021-09-01 17:32:13 +0300

    добавил localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contatsReduser from './reduser';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+const persistedReducer = persistReducer(persistConfig, contatsReduser);
 const store = configureStore({
   reducer: {
-    contacts: contatsReduser,
+    contacts: persistedReducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
 });
-
-export { store };
+const persistor = persistStore(store);
+export { store, persistor };
